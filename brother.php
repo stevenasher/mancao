1e8caba0a5328e0e56363f64f3886e9a7d6b264e
stevenasher <36062665+stevenasher@users.noreply.github.com>
2018-04-14 18:57:44 -0700

    Update brother.php

--- brother.php
+++ brother.php
@@ -15,7 +15,7 @@ body{
 </style>
 </head>
 <body>
-<a href=" content.php" target="content"><b>GO TO HOME</b></a></li>
+<a href="content.php" target="content"><b>GO TO HOME</b></a></li>
 <h1><center><b>MY BROTHER's</b></h1></center>
 <center><a href="jc.php" target="content"><mark><b>UGEE SUARING MANCAO</mark></a>
 |<a href="lj.php" target="content"><mark><b>JEZREEL SUARING MANCAO<mark></a>
